a5894e0515d7704f968c3ea695fe56225e19d890
Colin Moldenhauer <colin.moldenhauer@posteo.de>
2025-09-23 00:34:51 +0200

    Hatch setup

---
+++ src/qgis_project/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2025-present Colin Moldenhauer <colin.moldenhauer@posteo.de>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ src/qgis_project/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2025-present Colin Moldenhauer <colin.moldenhauer@posteo.de>
+#
+# SPDX-License-Identifier: MIT
