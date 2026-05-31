2f5df66fd47707bab675b829bf585116ce1f5986
Colin Moldenhauer <colin.moldenhauer@posteo.de>
2026-05-31 16:23:18 +0200

    Set up package for PyPI publishing
    pyproject.toml:
    - Add description, keywords
    - Fix classifiers (pre-alpha, CPython only, GIS topic, license)
    - Fix GitHub URLs (had spaces in username)
    - Add [project.optional-dependencies] dev group (pytest, mypy, coverage)
    - Add hatch default env with test/cov scripts
    - Document QGIS install strategy in comment (not on PyPI)
    
    __init__.py:
    - Switch to relative imports so package works when installed
    - Update to renamed style classes (Single/MultiPseudocolor)

--- src/qgis_project/__init__.py
+++ src/qgis_project/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2025-present Colin Moldenhauer <colin.moldenhauer@posteo.de>
 #
 # SPDX-License-Identifier: MIT
-from project import Project
-from layer import Layer, RasterLayer
-from style import Style, RasterStyle, RasterStyleBW, RasterStylePseudocolor
+from .project import Project
+from .layer import Layer, RasterLayer
+from .style import Style, RasterStyle, RasterStyleBW, RasterStyleSinglePseudocolor, RasterStyleMultiPseudocolor
